c516e32e6a6f1f15f74ca70cb89730fba6d16b1c
Pujitha Mule <pujithamule06@gmail.com>
2026-09-04 12:16:52 +0530

    Create majorityelement.py

---
+++ majorityelement.py
@@ -0,0 +1,14 @@
+def majorityElement(nums):
+    candidate = None
+    count = 0
+    
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+    
+    return candidate
+
+# Example usage:
+nums = [2, 2, 1, 1, 1, 2, 2]
+print(majorityElement(nums))  # Output: 2
