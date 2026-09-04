c340bc1d7a7f19d7d4440528ab57a705b484c112
Pujitha Mule <pujithamule06@gmail.com>
2026-09-04 12:20:13 +0530

    Create maximumelement.py

---
+++ maximumelement.py
@@ -0,0 +1,19 @@
+def maximumSwap(num):
+    # Convert number to list of digits
+    digits = list(str(num))
+    n = len(digits)
+    last = [0] * 10
+    for i in range(n):
+        last[int(digits[i])] = i
+
+    for i in range(n):
+        for d in range(9, int(digits[i]), -1):
+            if last[d] > i:
+                # Swap if we find a larger digit
+                digits[i], digits[last[d]] = digits[last[d]], digits[i]
+                return int(''.join(digits))
+    
+    return num  
+
+num = 2736
+print(maximumSwap(num))  # Output: 7236
