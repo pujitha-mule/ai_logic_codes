b9460874f07b4c42114ff31a4d3b942f4cfde368
Pujitha Mule <pujithamule06@gmail.com>
2026-09-04 12:47:31 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -13,6 +13,6 @@ output = two_sum(nums, target)
 print(output)
 
 
-#Time Complexity: O(n) — I only iterate through the nums list a single time. 
+#Time Complexity: O(n) - I only iterate through the nums list a single time. 
 #Inside the loop, checking if the complement exists in the num_to_index dictionary and inserting a new key-value pair both take O(1) average time.
-#Space Complexity: O(n) — In the worst-case scenario.its O(1) as we didn.t use any extra memory
+#Space Complexity: O(n) - In the worst-case scenario.its O(1) as we didn.t use any extra memory
