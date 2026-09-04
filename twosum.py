9551d644e629305b06cead5c0c2cf7a4a8333d31
Pujitha Mule <pujithamule06@gmail.com>
2026-09-04 12:32:21 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,21 +1,13 @@
 def two_sum(nums, target):
     num_to_index = {}
-    for index, num in enumerate(nums):
-         complement = target - num 
-         if complement in num_to_index: 
-             return [num_to_index[complement], index]
-             num_to_index[num] = index
-nums = [2, 7, 11, 15] 
-target = 9 
-output = two_sum(nums, target) 
-print(output)
-
-def two_sum(nums, target):
-    num_to_index = {}
     
     for index, num in enumerate(nums):
         complement = target - num
         if complement in num_to_index:
             return [num_to_index[complement], index]
         num_to_index[num] = index
-
+        
+nums = [2, 7, 11, 15] 
+target = 9 
+output = two_sum(nums, target) 
+print(output)
